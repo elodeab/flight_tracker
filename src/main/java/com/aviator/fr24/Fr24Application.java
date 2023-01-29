4f5126975784665c2dbd93b19e7757e4556da69a
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2023-01-29 23:54:27 +0300

    initializer files added

---
+++ src/main/java/com/aviator/fr24/Fr24Application.java
@@ -0,0 +1,13 @@
+package com.aviator.fr24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fr24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fr24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aviator/fr24/Fr24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviator.fr24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fr24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
